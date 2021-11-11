ae337017f35ea86dd3a6595fa7237cbfe974f75a
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-11 12:00:16 -0700

    Create contractorsPage.js

---
+++ contractorsPage.js
@@ -0,0 +1,10 @@
+import MainNavigation from "../components/mainNavigation";
+
+export default function Contractors() {
+  return (
+    <>
+      <MainNavigation />
+      <h1>ContrACTorS</h1>
+    </>
+  )
+}
