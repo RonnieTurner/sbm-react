c7e5bcd4f20988d5e9cc10b13413ae9fd9a30d5a
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-19 08:16:38 -0700

    Create AddClient.js

---
+++ components/AddClient/AddClient.js
@@ -0,0 +1,12 @@
+import ClientForm from "./ClientForm";
+import Card from "../ui/Card"
+
+export default function AddClient() {
+  return (
+    <div>
+      <Card>
+      <ClientForm />
+      </Card>
+    </div>
+  );
+}
