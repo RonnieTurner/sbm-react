e3618c8cc4b1d215cc507cbe0ed2b182780abc49
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-19 08:15:36 -0700

    Create AddContractor.js

---
+++ components/AddContractor/AddContractor.js
@@ -0,0 +1,12 @@
+import ContractorForm from "./ContractorForm";
+import Card from "../ui/Card"
+
+export default function AddContractor() {
+  return (
+    <div>
+      <Card>
+        <ContractorForm />
+      </Card>
+    </div>
+  )
+}
