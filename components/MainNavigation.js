103e353787c84430957d5b7b533e2517d12f0a87
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-19 08:20:57 -0700

    Update MainNavigation.js

--- components/MainNavigation.js
+++ components/MainNavigation.js
@@ -1,25 +1,40 @@
 import Link from "next/link";
-import styles from "./mainNavigation.module.css";
+import styles from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
   return (
     <>
       <ul className={styles.ul}>
-        <li>
+        <div>
           <Link href="/">
             <a>HoME</a>
           </Link>
-        </li>
-        <li>
+        </div>
+        <div>
           <Link href="/clients">
             <a>ClieNTS</a>
           </Link>
-        </li>
-        <li>
+        </div>
+        <div>
           <Link href="/contractors">
             <a>CoNTraCTorS</a>
           </Link>
-        </li>
+        </div>
+        <div>
+          <Link href="/AddProducts">
+            <a>Add PRoDucTS</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/AddNewClient">
+            <a>Add New Client</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/AddContractor">
+            <a>Add Contractor</a>
+          </Link>
+        </div>
       </ul>
     </>
   );
