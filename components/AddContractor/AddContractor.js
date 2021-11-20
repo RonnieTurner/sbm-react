d08410cda14ff1ac957cb5f09012a3cd84312281
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-20 09:42:42 -0700

    Update AddContractor.js

--- components/AddContractor/AddContractor.js
+++ components/AddContractor/AddContractor.js
@@ -1,11 +1,19 @@
 import ContractorForm from "./ContractorForm";
 import Card from "../ui/Card"
 
-export default function AddContractor() {
+export default function AddContractor(props) {
+  const saveContractorDataHandler = (enteredContractorData) => {
+    const contractorData = {
+      ...enteredContractorData,
+      id: Math.random().toString(),
+    }
+    props.onAddContractor(contractorData)
+  }
+
   return (
     <div>
       <Card>
-        <ContractorForm />
+        <ContractorForm onSaveContractorData={saveContractorDataHandler} />
       </Card>
     </div>
   )
