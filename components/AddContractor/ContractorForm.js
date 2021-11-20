6c735a081dcf41297fba9a6e4877df1e523fffa0
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-20 09:43:18 -0700

    Update ContractorForm.js

--- components/AddContractor/ContractorForm.js
+++ components/AddContractor/ContractorForm.js
@@ -1,46 +1,66 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function ContractorForm() {
-  const [enteredFName, setFName] = useState('');
-  const [enteredLName, setLName] = useState('');
-  const [enteredStreet, setStreet] = useState('');
-  const [enteredCity, setCity] = useState('');
-  const [enteredState, setState] = useState('');
-  const [enteredPhoneNumber, setPhoneNumber] = useState('');
-  const [enteredEmail, setEmail] = useState('');
+export default function ContractorForm(props) {
+  const [enteredFName, setFName] = useState("");
+  const [enteredLName, setLName] = useState("");
+  const [enteredStreet, setStreet] = useState("");
+  const [enteredCity, setCity] = useState("");
+  const [enteredState, setState] = useState("");
+  const [enteredPhoneNumber, setPhoneNumber] = useState("");
+  const [enteredEmail, setEmail] = useState("");
 
-  const setFNameHandler = event => {
+  const setFNameHandler = (event) => {
     setFName(event.target.value);
-  }
+  };
 
-  const setLNameHandler = event => {
+  const setLNameHandler = (event) => {
     setLName(event.target.value);
-  }
+  };
 
-  const setStreetHandler = event => {
-    setStreet(event.target.value)
-  }
+  const setStreetHandler = (event) => {
+    setStreet(event.target.value);
+  };
 
-  const setCityHandler = event => {
-    setCity(event.target.value)
-  }
+  const setCityHandler = (event) => {
+    setCity(event.target.value);
+  };
 
-  const setStateHandler = event => {
-    setState(event.target.value)
-  }
+  const setStateHandler = (event) => {
+    setState(event.target.value);
+  };
 
-  const setPhoneNumberHandler = event => {
-    setPhoneNumber(event.target.value)
-  }
+  const setPhoneNumberHandler = (event) => {
+    setPhoneNumber(event.target.value);
+  };
 
-  const setEmailHandler = event => {
-    setEmail(event.target.value)
-  }
-  
+  const setEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
 
+    const contractorData = {
+      firstName: enteredFName,
+      lastName: enteredLName,
+      street: enteredStreet,
+      city: enteredCity,
+      state: enteredState,
+      phoneNumber: enteredPhoneNumber,
+      email: enteredEmail,
+    };
+    props.onSaveContractorData(contractorData);
+
+    setFName("");
+    setLName("");
+    setStreet("");
+    setCity("");
+    setState("");
+    setPhoneNumber("");
+    setEmail("");
+  };
 
   return (
-    <div>
+    <form onSubmit={submitHandler}>
       <div>
         <label>First Name</label>
         <input type="text" value={enteredFName} onChange={setFNameHandler} />
@@ -73,6 +93,9 @@ export default function ContractorForm() {
         <label>Email Address</label>
         <input type="text" value={enteredEmail} onChange={setEmailHandler} />
       </div>
-    </div>
+      <div>
+        <button type="submit">Add New Contractor</button>
+      </div>
+    </form>
   );
 }
